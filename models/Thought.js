ee998f9201edfa88d2a4f0152ac8b26014d4f621
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-07-24 17:05:43 -0600

    user thought and user api

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,40 @@
-const { Schema, model, User } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)  
+        }    
+    }
+)
+
+
+
+
+
+
+
+
+
+
+
+
 
 const ThoughtSchema = new Schema(
     {
