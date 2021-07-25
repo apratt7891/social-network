405a6836e8d89014dbf2e18e04ad370e83e68dbb
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-07-24 19:08:04 -0600

    get thought by id

--- controllers/thought-controllers.js
+++ controllers/thought-controllers.js
@@ -11,20 +11,25 @@ const thoughtController = {
         res.status(400).json(err);
       });
   },
-// get one thought by id
- getThoughtById({ params }, res) {
-  Thought.findOne({ _id: params.id })
-      .populate({
-          path: 'user',
-          select: '-__v'
-      })
-     .select('-__v')
-     .sort({ _id: -1 })
-     .then(dbThoughtData => res.json(dbThoughtData))
-     .catch(err => {
-         console.log(err);
-         res.status(500).json(err)
-     })
+  // get a single thought by id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.thoughtId })
+        .populate({
+            path: 'reactions',
+            select: ('-__v')
+        })
+        .select('-__v')
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id' });
+                return;
+            }
+            res.json(dbThoughtData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
 },
     // create a thought
     createThought({ params, body }, res) {
